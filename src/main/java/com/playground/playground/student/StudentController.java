1bf5f7f9259048f99dc32b1dca2dd58481aecdf3
p8ul <pkinuthia10@gmail.com>
2021-03-13 07:59:33 +0300

    connect with postgres db

---
+++ src/main/java/com/playground/playground/student/StudentController.java
@@ -0,0 +1,40 @@
+package com.playground.playground.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/student")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+
+    }
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestBody Student updateStudent
+    ) {
+        studentService.updateStudent(studentId, updateStudent.getName(), updateStudent.getEmail());
+
+    }
+}
